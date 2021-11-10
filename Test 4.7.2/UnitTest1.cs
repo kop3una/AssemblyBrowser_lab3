5340b9b4af8b031c71038c2a985d54bce75c4b7a
kop3una <kirillalbert54@gmail.com>
2021-11-10 17:04:35 +0300

    finish

---
+++ Test 4.7.2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using AssemblyBrowserLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Test_4._7._2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string value = "C:\\Users\\kiril\\OneDrive\\Рабочий стол\\Учеба\\3 курс\\СПП\\lab3\\TestLibrary\\bin\\Debug\\TestLibrary.dll";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AssemblyBrowser assemblyBrowser = new AssemblyBrowser();
+            List<ContainerInfo> container = new List<ContainerInfo>(assemblyBrowser.GetNamespaces(value));
+            Assert.IsTrue(container[0].DeclarationName.Equals("TestLibrary") && container[1].DeclarationName.Equals("TestLibrary.Extension"));
+        }
+    }
+}
---
+++ TestLibrary/Foo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class Foo
+    {
+        public double MyDouble { get; set; }
+
+        public int MyMethod(int param)
+        {
+            return param;
+        }
+    }
+}
